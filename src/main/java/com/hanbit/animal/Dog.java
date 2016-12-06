98b7f5720d09869d98d42031b9cb17443fc34677
5000 <5000@hb5000>
2016-12-06 17:01:17 +0900

    abstract 연습 및 keywords

--- src/main/java/com/hanbit/animal/Dog.java
+++ src/main/java/com/hanbit/animal/Dog.java
@@ -1,66 +1,20 @@
 package com.hanbit.animal;
 
-import org.apache.commons.lang3.StringUtils;
-
-public class Dog {
+public class Dog extends Animal {
 	
 	public static final String DEFAULT_NAME = "멍멍이";
 
-	private String name;
-	private final String kind;
-	private String color;
-	
-	private boolean sit;
-	
 	public Dog(String kind, String color) {
 		this(DEFAULT_NAME, kind, color);
 	}
 	
 	public Dog(String name, String kind, String color) {
-		this.name = name;
-		this.kind = kind;
-		this.color = color;
-	}
-	
-	@Override
-	public String toString() {
-		return name + ":" + kind + ":" + color;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		if (StringUtils.isBlank(name)) {
-			name = DEFAULT_NAME;
-		}
-		
-		this.name = name;
-	}
-	
-	public String getKind() {
-		return kind;
-	}
-	
-	public void setColor(String color) {
-		if (StringUtils.isBlank(color)) {
-			return;
-		}
-		
-		this.color = color;
+		super(name, kind, color);
 	}
 
-	public String getColor() {
-		return color;
-	}
-	
-	public void sitDown() {
-		sit = true;
-	}
-	
-	public void standUp() {
-		sit = false;
+	@Override
+	protected String getDefaultName() {
+		return DEFAULT_NAME;
 	}
 	
 }
--- src/main/java/com/hanbit/java/Runner.java
+++ src/main/java/com/hanbit/java/Runner.java
@@ -1,14 +1,19 @@
 package com.hanbit.java;
 
+import com.hanbit.animal.Cat;
 import com.hanbit.animal.Dog;
 
 public class Runner {
 	
 	public static void main(String[] args) {
 		Dog dog = new Dog("도베르만", "검정");
+		Cat cat = new Cat("길고양이", "줄무늬");
 		
-		dog.sitDown();
-		dog.standUp();
+		dog.setName("");
+		cat.setName(null);
+		
+		System.out.println(dog);
+		System.out.println(cat);
 	}
 	
 }
